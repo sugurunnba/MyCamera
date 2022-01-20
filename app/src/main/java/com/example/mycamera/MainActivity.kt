b4b71d6f4a8b4ec2549d39d3005898e420d8a919
sugurunnba <sugurunnba7@gmail.com>
2022-01-20 23:00:30 +0900

    プレビューボタン押下後、カメラアプリ起動。写真撮影後、暗黙インテントでActivityに撮影したデータを反映するようにした。

--- app/src/main/java/com/example/mycamera/MainActivity.kt
+++ app/src/main/java/com/example/mycamera/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.mycamera
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.example.mycamera.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_PREVIEW = 1
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//        ラジオボタンの選択状態が変更された時に呼ばれるリスナー
         binding.radioGroup.setOnCheckedChangeListener{ group, checkedId ->
             when (checkedId){
                 R.id.preview ->
@@ -21,12 +27,27 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.cameraButton.setOnClickListener{
+//            選択中のラジオボタンIDを返す
             when (binding.radioGroup.checkedRadioButtonId){
                 R.id.preview -> preview()
                 R.id.takePicture -> takePicture()
             }
         }
     }
-    private fun preview(){}
+    private fun preview(){
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+            intent.resolveActivity(packageManager)?.also {
+                startActivityForResult(intent, REQUEST_PREVIEW)
+            }
+        }
+    }
     private fun takePicture(){}
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_PREVIEW && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.imageView.setImageBitmap(imageBitmap)
+        }
+    }
 }
